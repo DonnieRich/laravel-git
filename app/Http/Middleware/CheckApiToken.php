50d5456b3bcca285671129e876a8ac26d9a6af2e
Donnie <donniepask@gmail.com>
2021-02-01 20:17:34 +0100

    Middleware logic and api route

--- app/Http/Middleware/CheckApiToken.php
+++ app/Http/Middleware/CheckApiToken.php
@@ -15,6 +15,24 @@ class CheckApiToken
      */
     public function handle($request, Closure $next)
     {
+        $auth_token = $request->header('authorization');
+        // verifico se è presente un token di autorizzazione
+        if(empty($auth_token)) {
+            return response()->json([
+                'success' => false,
+                'error' => 'Api token mancante'
+            ]);
+        }
+        // estraggo il token dall'header
+        $api_token = substr($auth_token, 7);
+        // verifico la correttezza dell'api_token
+
+        if($api_token !== "123456789") {
+            return response()->json([
+                'success' => false,
+                'error' => 'Api token errato'
+            ]);
+        }
         return $next($request);
     }
 }
